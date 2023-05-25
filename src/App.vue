cb83b058a6f57e2e12c5c4ffd679603360d4c38b
Dima <skaizy44@gmail.com>
2023-05-25 17:24:10 +0300

    jyj
    yjyj

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>HELLO DIMA</p>
+  <VueList></VueList>
+</template>
+
+
+<script>
+import VueList from "./components/VueList.vue";
+export default {
+  components: {
+    VueList: VueList,
+  },
+};
+</script>
+
+<style>
+</style>
+
+
---
+++ src/components/VueList.vue
@@ -0,0 +1,3 @@
+<template>
+    <p>my list</p>
+</template>
