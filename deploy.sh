d6360c817a25e8d995dc0ff233f2bc7fd64e70ff
Dima <skaizy44@gmail.com>
2023-06-01 20:57:22 +0300

    uy
    uyuy

--- deploy.sh
+++ deploy.sh
@@ -21,5 +21,5 @@ git commit -m 'deploy'
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-
+git push -f git@github.com:dima-kulikov/vue3-vtwitter.git master:gh-pages
 cd -
